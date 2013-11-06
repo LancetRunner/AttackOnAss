660cdf57e284bf8a8ba7acccbec88d6fea33bb3a
LancetRunner <linpeng@Lins-iMac.Atenpl.local>
2013-11-06 17:54:55 +0800

    Initial Commit

---
+++ AttackOnAss/AppDelegate.h
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.h
+//  AttackOnAss
+//
+//  Created by Lin Peng on 6/11/13.
+//  Copyright FunWorks 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+// Added only for iOS 6 support
+@interface MyNavigationController : UINavigationController <CCDirectorDelegate>
+@end
+
+@interface AppController : NSObject <UIApplicationDelegate>
+{
+	UIWindow *window_;
+	MyNavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) MyNavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
---
+++ AttackOnAss/IntroLayer.h
@@ -0,0 +1,21 @@
+//
+//  IntroLayer.h
+//  AttackOnAss
+//
+//  Created by Lin Peng on 6/11/13.
+//  Copyright FunWorks 2013. All rights reserved.
+//
+
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface IntroLayer : CCLayer
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
